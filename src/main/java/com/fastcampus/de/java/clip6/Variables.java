992c8ef672d4ca77c2cdc333a871ab9c1d36966b
HaJunYoo <hajuny129@gmail.com>
2022-12-08 00:43:58 +0900

    ✨ feat: 2022/12/8

---
+++ src/main/java/com/fastcampus/de/java/clip6/Variables.java
@@ -0,0 +1,19 @@
+package com.fastcampus.de.java.clip6;
+
+public class Variables {
+    public static void main(String[] args) {
+        int number = 1;
+        System.out.println(number);
+
+        String str = "Hello World!"; // 문자형 변수 선언
+        System.out.println(str);
+
+        str = "Goodbye World"; // 이미 선언된 변수에 다른 값을 할당
+        System.out.println(str);
+
+        final int num2 = 10; // 상수형 변수 선언 (final로 선언 가능)
+        System.out.println(num2);
+        // num2 = 50;
+
+    }
+}
---
+++ src/main/java/com/fastcampus/de/java/clip7/Numbers.java
@@ -0,0 +1,34 @@
+package com.fastcampus.de.java.clip7;
+
+public class Numbers {
+    public static void main(String[] args){
+        short s = 1;
+        System.out.println(s);
+
+        int a = 3; // 정수형 변수 선언
+        System.out.println(a);
+
+        long b = 1234567890L; // Long 정수형 변수 선언
+        System.out.println(b);
+
+        float c = 5.5F; // float 실수형 변수 선언
+        System.out.println(c);
+
+        double d = 9.12345678901234567890d; // double 실수형 변수 선언
+        System.out.println(d);
+
+        // 다음처럼 각 자료형의 MAX, MIN 값을 가져올 수 있습니다.
+        System.out.println(Short.MAX_VALUE);
+        System.out.println(Short.MIN_VALUE);
+        System.out.println(Integer.MAX_VALUE);
+        System.out.println(Integer.MIN_VALUE);
+        System.out.println(Long.MAX_VALUE);
+        System.out.println(Long.MIN_VALUE);
+        System.out.println(Float.MAX_VALUE);
+        System.out.println(Float.MIN_VALUE);
+        System.out.println(Double.MAX_VALUE);
+        System.out.println(Double.MIN_VALUE);
+        // 모든 number type의 default value는 0(또는 0.0) 입니다.
+    }
+
+}
